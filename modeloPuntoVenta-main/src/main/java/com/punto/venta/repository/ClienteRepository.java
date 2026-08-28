868f8f7035996d4590a8440a73a9e68c9a18611f
ervinlopez811-droid <ervinlopez811@gmail.com>
2026-08-27 21:59:37 -0600

    Delete modeloPuntoVenta-main directory

--- modeloPuntoVenta-main/src/main/java/com/punto/venta/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.punto.venta.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.punto.venta.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    boolean existsByNombreIgnoreCaseAndApellidoIgnoreCase(String nombre, String apellido);
-}
--- modeloPuntoVenta-main/src/main/java/com/punto/venta/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.punto.venta.repository;
-
-import com.punto.venta.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
