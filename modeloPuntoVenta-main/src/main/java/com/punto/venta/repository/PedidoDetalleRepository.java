b1d236ae02b3b13b5cefe7d4a513aa5ce9a0bdbe
ervinlopez811-droid <ervinlopez811@gmail.com>
2026-08-27 21:57:30 -0600

    Add files via upload

---
+++ modeloPuntoVenta-main/src/main/java/com/punto/venta/repository/PedidoDetalleRepository.java
@@ -0,0 +1,9 @@
+package com.punto.venta.repository;
+
+import com.punto.venta.entity.PedidoDetalle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoDetalleRepository extends JpaRepository<PedidoDetalle, Long> {
+}
